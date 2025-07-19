797b6de9daeda76bd771ef5ff995ae6f3426b0f5
ilyas-ben <ilyasben313@gmail.com>
2025-07-19 13:12:03 +0100

    initialize customer module

---
+++ customerservice/src/main/java/com/ilouse/CustomerApplication.java
@@ -0,0 +1,19 @@
+package com.ilouse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Hello world!
+ *
+ */
+
+
+@SpringBootApplication
+public class CustomerApplication
+{
+    public static void main( String[] args )
+    {
+        SpringApplication.run(CustomerApplication.class, args);
+    }
+}
