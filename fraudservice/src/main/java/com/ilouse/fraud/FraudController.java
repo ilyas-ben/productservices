ecd447754d9908b456d8a8ce831d5adb3046b1cf
ilyas-ben <ilyasben313@gmail.com>
2025-07-20 13:12:41 +0100

    update controller

--- fraudservice/src/main/java/com/ilouse/fraud/FraudController.java
+++ fraudservice/src/main/java/com/ilouse/fraud/FraudController.java
@@ -4,7 +4,7 @@ import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("fraud-check")
+@RequestMapping("fraudcheck")
 @AllArgsConstructor
 public class FraudController {
 
